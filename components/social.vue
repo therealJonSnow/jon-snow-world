41cb50e20eb7a6adb0e24cbd9e6de121d20f904a
Jon Snow <47143928+therealJonSnow@users.noreply.github.com>
2022-02-22 13:35:47 +0000

    Update icons

--- components/social.vue
+++ components/social.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="absolute bottom-0 space-x-16 left-1/2 -translate-x-1/2 social shadow-sm px-6 py-3">
         <a class="flex items-center justify-center" href="https://www.instagram.com/its_jonny_snow/" target="_blank">
-            <fa :icon="['fab', 'instagram']"/>
+            <img class="w-8 h-8" src="../assets/images/instagram-brands.svg"/>
         </a>
         <a class="flex items-center justify-center" href="https://dribbble.com/itsJS" target="_blank">
-            <fa :icon="['fab', 'dribbble']"/>
+            <img class="w-8 h-8" src="../assets/images/dribbble-brands.svg"/>
         </a>
         <a class="flex items-center justify-center" href="https://www.linkedin.com/in/jonathan-snow-452753150/" target="_blank">
-            <fa :icon="['fab', 'linkedin']"/>
+            <img class="w-8 h-8" src="../assets/images/linkedin-in-brands.svg"/>
         </a>
     </div>
 </template>
